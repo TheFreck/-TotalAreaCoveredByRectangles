1ff8baec8972d40057994cd6154d90c16b4bb9b0
Chris Freckleton <36802104+TheFreck@users.noreply.github.com>
2024-03-29 18:13:47 -0600

    a few optimizations

--- TotalAreaCoveredByRectangles/TotalAreaCoveredByRectangles.Specs/UnitTest1.cs
+++ TotalAreaCoveredByRectangles/TotalAreaCoveredByRectangles.Specs/UnitTest1.cs
@@ -1,5 +1,6 @@
 using Machine.Specifications;
 using System.Collections;
+using System.Diagnostics;
 
 namespace TotalAreaCoveredByRectangles.Specs
 {
@@ -9,9 +10,10 @@ namespace TotalAreaCoveredByRectangles.Specs
         {
             input = new List<int[]>
             {
-                new [] {0, 4, 11, 6}
+                new int[]{0,0,10,10},
+                new int[]{11,0,21,21}
             };
-            expect = 22;
+            expect = 310;
         };
 
         Because of = () => answer = AreaFinder.Calculate(input);
@@ -19,6 +21,110 @@ namespace TotalAreaCoveredByRectangles.Specs
         It Should_Return_An_Integer_Area = () => answer.ShouldEqual(expect);
 
         private static IEnumerable<int[]> input;
+        private static int expect;
+        private static long answer;
+    }
+
+    public class When_Accounting_For_Overlaps
+    {
+        Establish context = () =>
+        {
+            input = new List<int[]>
+            {
+                new [] {0, 0, 10, 10},
+                new [] {5, 0, 15, 10}
+            };
+            expect = 150;
+        };
+
+        Because of = () => answer = AreaFinder.Calculate(input);
+
+        It Should_Return_An_Integer_Area = () => answer.ShouldEqual(expect);
+
+        static List<int[]> input;
+        static int expect;
+        private static long answer;
+    }
+
+    public class When_Trying_Without_Rectangles
+    {
+        Establish context = () =>
+        {
+            input = new List<int[]>
+            {
+
+            };
+            expect = 0;
+        };
+
+        Because of = () => answer = AreaFinder.Calculate(input);
+
+        It Should_Return_An_Integer_Area = () => answer.ShouldEqual(expect);
+
+        private static IEnumerable<int[]> input;
+        private static int expect;
+        private static long answer;
+
+    }
+
+    public class When_Generating_Many_Rectangles
+    {
+        Establish context = () =>
+        {
+            input = 10;
+        };
+
+        Because of = () => answer = AreaFinder.GenerateRectangles(input);
+
+        It Should_Return_Expected_Number_Of_Rectangles = () => answer.Count.ShouldEqual(input);
+
+        static int input;
+        static List<int[]> answer;
+    }
+
+    public class When_Trying_So_So_Many_Rectangles
+    {
+        Establish context = () =>
+        {
+            input = AreaFinder.GenerateRectangles(15000);
+
+        };
+
+        Because of = () =>
+        {
+            stopwatch = new Stopwatch();
+            stopwatch.Start();
+            answer = AreaFinder.Calculate(input);
+            stopwatch.Stop();
+            elapsedTime = stopwatch.ElapsedMilliseconds;
+        };
+
+        It Should_Have_Greater_Than_Zero_Area = () => answer.ShouldBeGreaterThan(0);
+
+        static List<int[]> input;
+        static Stopwatch stopwatch;
+        static long answer;
+        static long elapsedTime;
+    }
+
+    public class When_Accounting_For_Rectangles_Covered_Completely_By_Other_Rectangles
+    {
+        Establish context = () =>
+        {
+            input = new List<int[]>
+            {
+                new int[]{0,0,10,10},
+                new int[]{1,1,9,9},
+                new int[]{0,5,11,10}
+            };
+            expect = 105;
+        };
+
+        Because of = () => answer = AreaFinder.Calculate(input);
+
+        It Should_Return_Expected_Area = () => answer.ShouldEqual(expect);
+
+        private static List<int[]> input;
         private static int expect;
         private static long answer;
     }
--- TotalAreaCoveredByRectangles/TotalAreaCoveredByRectangles/AreaFinder.cs
+++ TotalAreaCoveredByRectangles/TotalAreaCoveredByRectangles/AreaFinder.cs
@@ -10,16 +10,36 @@ namespace TotalAreaCoveredByRectangles
     {
         public static long Calculate(IEnumerable<int[]> rectangles)
         {
-            var areas = new List<int>();
-            foreach(var rectangle in rectangles)
+            var intsEnumerable = rectangles as int[][] ?? rectangles.ToArray();
+            if (intsEnumerable.ToList().Count <= 0) return 0;
+            var hash = new HashSet<string>();
+            var fakeHash = new List<string>();
+            long area = 0;
+            for (var x = intsEnumerable.Min(r => Math.Min(r[0]+1, r[2])); x <= intsEnumerable.Max(r => Math.Max(r[0],r[2])); x++)
             {
-                var topLeft = Tuple.Create(rectangle[0], rectangle[1]);
-                var topRight = Tuple.Create(rectangle[2], rectangle[1]);
-                var bottomLeft = Tuple.Create(rectangle[0], rectangle[3]);
-                var bottomRight = Tuple.Create(rectangle[2], rectangle[3]);
-                areas.Add((topRight.Item1 - topLeft.Item1) * (bottomRight.Item2 - bottomLeft.Item2));
+                var items = intsEnumerable.Where(r => Math.Min(r[0], r[2]) < x && Math.Max(r[0], r[2]) >= x).ToArray();
+                long upperRange = items.Length > 0 ? items.Max(r => Math.Max(r[1], r[3])) : 0;
+                long lowerRange = items.Length > 0 ? items.Min(r => Math.Min(r[1], r[3])) : 0;
+                area += upperRange > 0 && upperRange > lowerRange ? upperRange-lowerRange : 0;
             }
-            return areas.Sum();
+
+            return area;
+        }
+
+        public static List<int[]> GenerateRectangles(int input)
+        {
+            var randy = new Random();
+            var rectangles = new List<int[]>();
+            for (var i = 0; i < input; i++)
+            {
+                var x1 = randy.Next(1000000);
+                var y1 = randy.Next(1000000);
+                var x2 = randy.Next(x1, 1000000);
+                var y2 = randy.Next(y1, 1000000);
+                rectangles.Add([x1, y1, x2, y2]);
+            }
+
+            return rectangles;
         }
     }
 }
